fdc6f1d155c895ee594bdf908f63577e7ae60d7b
Francesco Murdaca <francesco.murdaca91@gmail.com>
2026-04-24 16:04:30 +0200

    chore: release 0.3.1 (#103)
    Co-authored-by: Francesco Murdaca <francesco.murdaca@eumetsat.int>

--- ewccli/__init__.py
+++ ewccli/__init__.py
@@ -8,7 +8,7 @@
 """Initialize ewccli package."""
 
 __title__ = "ewccli"
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 __author__ = "European Weather Cloud <support@europeanweather.cloud>"
 
 __all__ = [
